bb0909931adbb156a9a30d3299d874f15f2c2312
snifee <razyaarf@gmail.com>
2023-02-03 11:38:01 +0800

    change transaction to transactions

--- src/main/java/com/kostserver/controller/TransactionController.java
+++ src/main/java/com/kostserver/controller/TransactionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/v1/transaction")
+@RequestMapping("/v1/transactions")
 public class TransactionController {
 
     @Autowired
