2ed398eaa89b7d93fc851483df1ced95d8e5ab87
snifee <razyaarf@gmail.com>
2023-02-11 04:18:24 +0800

    merge!

--- src/main/java/com/kostserver/model/EnumKostPaymentScheme.java
+++ src/main/java/com/kostserver/model/EnumKostPaymentScheme.java
@@ -3,7 +3,14 @@ package com.kostserver.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum EnumKostPaymentScheme {
-    BULANAN,HARIAN,MINGGUAN;
+    BULANAN(30),HARIAN(1),MINGGUAN(7),PER_6BULAN(180),PER_3BULAN(90),TAHUNAN(365);
+
+    public final Integer days;
+
+    private EnumKostPaymentScheme(Integer days) {
+        this.days = days;
+    }
+
     @JsonCreator
     public static EnumKostPaymentScheme getTypeFromCode(String value) {
 
